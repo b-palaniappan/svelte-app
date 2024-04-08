a6ee0d4d2775060d81b36302a0772d48932198b4
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2024-04-07 21:42:14 -0500

    added shadcn card and light/dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
 	import '@fontsource/poppins';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
 	import * as Select from '$lib/components/ui/select/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
+	import Sun from 'svelte-radix/Sun.svelte';
+	import Moon from 'svelte-radix/Moon.svelte';
+	import { toggleMode } from 'mode-watcher';
 
 	const frameworks = [
 		{
@@ -25,38 +28,53 @@
 	];
 </script>
 
-<Card.Root class="w-[350px]">
-	<Card.Header>
-		<Card.Title>Create project</Card.Title>
-		<Card.Description>Deploy your new project in one-click.</Card.Description>
-	</Card.Header>
-	<Card.Content>
-		<form>
-			<div class="grid w-full items-center gap-4">
-				<div class="flex flex-col space-y-1.5">
-					<Label for="name">Name</Label>
-					<Input id="name" placeholder="Name of your project" />
+<!-- Toggle Light and Dark modes -->
+<div class="md:container md:mx-auto">
+	<div class="flex justify-end">
+		<Button on:click={toggleMode} variant="outline" size="icon">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</div>
+	<!-- Card component -->
+	<Card.Root class="w-[350px]">
+		<Card.Header>
+			<Card.Title>Create project</Card.Title>
+			<Card.Description>Deploy your new project in one-click.</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<form>
+				<div class="grid w-full items-center gap-4">
+					<div class="flex flex-col space-y-1.5">
+						<Label for="name">Name</Label>
+						<Input id="name" placeholder="Name of your project" />
+					</div>
+					<div class="flex flex-col space-y-1.5">
+						<Label for="framework">Framework</Label>
+						<Select.Root>
+							<Select.Trigger id="framework">
+								<Select.Value placeholder="Select" />
+							</Select.Trigger>
+							<Select.Content>
+								{#each frameworks as framework}
+									<Select.Item value={framework.value} label={framework.label}
+										>{framework.label}</Select.Item
+									>
+								{/each}
+							</Select.Content>
+						</Select.Root>
+					</div>
 				</div>
-				<div class="flex flex-col space-y-1.5">
-					<Label for="framework">Framework</Label>
-					<Select.Root>
-						<Select.Trigger id="framework">
-							<Select.Value placeholder="Select" />
-						</Select.Trigger>
-						<Select.Content>
-							{#each frameworks as framework}
-								<Select.Item value={framework.value} label={framework.label}
-									>{framework.label}</Select.Item
-								>
-							{/each}
-						</Select.Content>
-					</Select.Root>
-				</div>
-			</div>
-		</form>
-	</Card.Content>
-	<Card.Footer class="flex justify-between">
-		<Button variant="outline">Cancel</Button>
-		<Button>Deploy</Button>
-	</Card.Footer>
-</Card.Root>
+			</form>
+		</Card.Content>
+		<Card.Footer class="flex justify-between">
+			<Button variant="outline">Cancel</Button>
+			<Button>Deploy</Button>
+		</Card.Footer>
+	</Card.Root>
+</div>
